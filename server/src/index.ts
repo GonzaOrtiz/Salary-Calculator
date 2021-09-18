646116cab9459acf4270cbb1780afcfc08bb79e4
Gonza Ortiz <gonzaortiz1506@gmail.com.com>
2021-09-17 21:36:15 -0300

    fixing index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,10 @@
 import express from 'express';
 import { log } from 'debug';
+import cors from 'cors';
+import expressWinston from 'express-winston';
+import winston from 'winston';
+import CommonRoutes from './http/routes/common.routes';
+import RateRoutes from './http/routes/rate.routes';
 
 const app: express.Application = express();
 
@@ -7,6 +12,32 @@ app.get('/', (_req: express.Request, res: express.Response) => {
   res.send('Hola');
 })
 
+const loggerOptions: expressWinston.LoggerOptions = {
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+      winston.format.json(),
+      winston.format.prettyPrint(),
+      winston.format.colorize({ all: true })
+  ),
+};
+
+if (!process.env.DEBUG) {
+    loggerOptions.meta = false; // when not debugging, log requests as one-liners
+}
+
+app.use(expressWinston.logger(loggerOptions));
+
+
+
+const routes: Array<CommonRoutes> = [];
+app.use(cors());
+app.use(express.json());
+
+routes.push(new RateRoutes(app));
+
 app.listen(3000, () => {
+  routes.forEach((route: CommonRoutes) => {
+    log(`Routes configured for ${route.getName()}`);
+  });
   log('Server listening on port 3000');
 });
