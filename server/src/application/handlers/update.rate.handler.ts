cf44b5fbd692ac21595529ea0f782dc297417ea4
Leonel Cassani <cassanileo@gmail.com>
2021-10-15 19:36:46 -0300

    Update

---
+++ server/src/application/handlers/update.rate.handler.ts
@@ -0,0 +1,17 @@
+import  Rate  from "../../domain/entities/rate.entity";
+import CreateRateCommand from "../commands/create.rate.command";
+import RateRepository from "../../infrastructure/repositories/rate.repository";
+
+/*class UpdateRateHandler {
+   async execute(command: ) {
+       const user = await ().findOneById();
+
+        if () {
+            throw new Error();
+        } 
+
+       // await userRepository.save(user);
+    }
+} */
+
+export default new UpdateRateHandler();
